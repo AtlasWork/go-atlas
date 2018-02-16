830600156dd1ba0834d7ff1beaada2b8f6bf9b20
Patrick-Joseph: Lismore <patricklismore@gmail.com>
2018-02-16 00:44:03 +0000

    updated bootnodes

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -21,6 +21,7 @@ package params
 var MainnetBootnodes = []string{
 	// Atlas Foundation Go Bootnodes
 	"enode://353aa4f73e5cd159ee32d3d8586bea7664ec6d0fb414dd0661f9c9e66412c46b42c30f0ba17143693e689ea4adc7b11560fd6ed309752ba6989f48fc1941633c@35.169.121.206:57200",
+	"enode://112b38abd08daed1d10ebfdc268e0c0170a0c4c7127ec5b62a3cc8ffca2eba1c7e4475b1a6d68299e521d00dde946970a613f03ca1f0c5bbac6db0ae9c7d7b28@34.232.219.29:57200",
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
